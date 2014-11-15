1ba8ef716774b8ee7f1d297d20d0685b5b334360
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-11-16 00:05:56 +0100

    First commit: application structure

---
+++ app/src/evtviewer.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name evtviewerApp
+ * @description
+ * # evtviewerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('evtviewerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'templates-main'
+  ]);
