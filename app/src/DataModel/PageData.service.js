3653a1696cc0fbb35a39c2f99006bed62fbef770
Raffaele Masotti <raffaele.masotti@gmail.com>
2014-11-29 17:19:16 +0100

    Minor change

--- app/src/DataModel/PageData.service.js
+++ app/src/DataModel/PageData.service.js
@@ -1,7 +1,7 @@
 angular.module('evtviewer.dataModel')
 
 .service('PageData', function(BaseComponent) {
-    var pageData = new BaseComponent('PageData'),
+    var pageData = new BaseComponent('PageData');
 
     return pageData;
 });
