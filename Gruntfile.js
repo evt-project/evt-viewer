afda4368ce53167e534d87f1d534ce156ae51844
Ilaria Tiezzi <ilaria.tiezzi@gmail.com>
2015-03-18 21:11:48 +0100

    Grunt livereload port change

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 37729
       },
       livereload: {
         options: {
