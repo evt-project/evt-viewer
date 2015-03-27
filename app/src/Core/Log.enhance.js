a6daf4a2675ff87b8b3e61fe3daba6c7c6ac9ca4
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-03-27 23:57:01 +0000

    Add $log enhance

---
+++ app/src/Core/Log.enhance.js
@@ -0,0 +1,56 @@
+/*jshint -W059 */
+
+angular.module('evtviewer.core')
+
+.run(function($log, Config) {
+    $log.enabledContexts = [];
+    $log.getInstance = function(context) {
+        return {
+            log: enhanceLog($log.log, context),
+            info: enhanceLog($log.info, context),
+            warn: enhanceLog($log.warn, context),
+            debug: enhanceLog($log.debug, context),
+            error: enhanceErr($log.error, context),
+            enableLogging: function(enable) {
+                $log.enabledContexts[context] = enable;
+            }
+        };
+    };
+
+    function enhanceLog(loggingFunc, context) {
+        return function() {
+            var contextEnabled;
+
+            if (typeof($log.enabledContexts[context]) !== 'undefined') {
+                contextEnabled = $log.enabledContexts[context];
+            } else {
+                contextEnabled = Config.modules[context].debug;
+            }
+
+            if (Config.debugAllModules === true || contextEnabled === true) {
+                var args = Array.prototype.slice.call(arguments);
+                args.unshift('#' + context + '#');
+                $log.log.apply(null, args);
+            }
+        };
+    }
+
+    function enhanceErr(loggingFunc, context) {
+        return function() {
+            var fileErr = function() {
+                try {
+                    throw Error('');
+                } catch (err) {
+                    return err;
+                }
+            };
+            var currentErr = fileErr();
+            var callerLine = currentErr.stack.split('\n')[5];
+
+            var args = Array.prototype.slice.call(arguments);
+            args.unshift('#EVTViewer ' + context + '#');
+            args.push(callerLine);
+            $log.error.apply(null, args);
+        };
+    };
+});
