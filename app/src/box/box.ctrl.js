1f76903085ec97423d8a2aedadd934aaa7575b04
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-03-28 22:36:21 +0000

    Minor change

--- app/src/box/box.ctrl.js
+++ app/src/box/box.ctrl.js
@@ -1,7 +1,6 @@
 angular.module('evtviewer.box')
 
 .controller('BoxCtrl', function($document, $window, $rootScope, $scope, $log, Box) {
-
     var _console = $log.getInstance('box');
     
     Box.addReference($scope);
