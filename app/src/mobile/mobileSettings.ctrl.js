3fefc860502e6aa4cef4967ea37326354de1df9e
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-04-03 21:27:24 +0200

    Minor fix jshint

--- app/src/mobile/mobileSettings.ctrl.js
+++ app/src/mobile/mobileSettings.ctrl.js
@@ -1,6 +1,6 @@
 angular.module('evtviewer.mobile')
 
-.controller('MobileSettingsCtrl', function($scope, parsedData) {
+.controller('MobileSettingsCtrl', function(/*$scope, parsedData*/) {
    
   // $scope.text = parsedData.getText();
 });
