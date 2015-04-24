c1eb71ecd6bddb051ca5c7b76c61f2a59bd1034c
Ilaria Tiezzi <ilaria.tiezzi@gmail.com>
2015-04-24 15:58:42 +0200

    Add first example of the section settings

--- app/src/mobile/mobileSettings.ctrl.js
+++ app/src/mobile/mobileSettings.ctrl.js
@@ -1,6 +1,18 @@
 angular.module('evtviewer.mobile')
 
-.controller('MobileSettingsCtrl', function(/*$scope, parsedData*/) {
-   
-  // $scope.text = parsedData.getText();
+.controller('MobileSettingsCtrl', function($scope, mobile) {
+
+    $scope.view = mobile.getState();
+
+    $scope.textOptions = false;
+    $scope.imageOptions = false;
+
+    $scope.showTextSettings = function() {
+        $scope.textOptions = !$scope.textOptions;
+    };
+
+    $scope.showImageSettings = function() {
+        $scope.imageOptions = !$scope.imageOptions;
+    };
+
 });
