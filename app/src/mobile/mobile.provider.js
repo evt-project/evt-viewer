7fe2340b5a7305b36656684a298f298ae576e4d9
Ilaria Tiezzi <ilaria.tiezzi@gmail.com>
2015-06-29 19:22:00 +0200

    Mobile provider improvement for text editions

--- app/src/mobile/mobile.provider.js
+++ app/src/mobile/mobile.provider.js
@@ -20,17 +20,51 @@ angular.module('evtviewer.mobile')
             currentView: 'image'
         };
 
+        var stateEditionDipl= {
+            currentEditionDipl: 'diplomatic'
+        };
+
+        var stateEditionInt= {
+            currentEditionInt: 'interpretative'
+        };
+
         mobile.getState = function() {
             return state;
+        };
+
+        mobile.getStateEditionDipl = function() {
+            return stateEditionDipl;
+        };
+
+        mobile.getStateEditionInt = function() {
+            return stateEditionInt;
         };
 
         mobile.getCurrentView = function() {
             return state.currentView;
         };
 
+        mobile.getCurrentEditionDipl = function() {
+            return stateEditionDipl.currentEditionDipl;
+        };
+
+        mobile.getCurrentEditionInt = function() {
+            return stateEditionInt.currentEditionInt;
+        };
+
         mobile.switchView = function(view) {
             state.currentView = view;
             _console.log('Switch of current view in: ' + view);
+        };
+
+        mobile.switchEditionDipl = function(editionDipl) {
+            stateEditionDipl.currentEditionDipl = editionDipl;
+            _console.log('Switch of current edition in: ' + editionDipl);
+        };
+
+        mobile.switchEditionInt = function(editionInt) {
+            stateEditionInt.currentEditionInt = editionInt;
+            _console.log('Switch of current edition in: ' + editionInt);
         };
 
         return mobile;
