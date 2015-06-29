1dee86f5c4ddfd43a0f0f985d1cdccf9a88a8af2
Ilaria Tiezzi <ilaria.tiezzi@gmail.com>
2015-06-29 19:29:55 +0200

    Add temporary mobile text page data and relative style

--- app/src/dataHandler/parsedData.service.js
+++ app/src/dataHandler/parsedData.service.js
@@ -90,6 +90,28 @@ angular.module('evtviewer.dataHandler')
         interpretative: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath',
     }];
 
+    var mockText2 = [{
+        title: 'The Dream of the Rood',
+        page: '104v',
+        diplomatic: './data/output_data/diplomatic/page_VB_fol_104v_diplomatic.html',
+        interpretative: './data/output_data/interpretative/page_VB_fol_104v_interpretative.html',
+    }, {
+        title: 'The Dream of the Rood',
+        page: '105r',
+        diplomatic: './data/output_data/diplomatic/page_VB_fol_105r_diplomatic.html',
+        interpretative: './data/output_data/interpretative/page_VB_fol_105r_interpretative.html',
+    }, {
+        title: 'The Dream of the Rood',
+        page: '105v',
+        diplomatic: './data/output_data/diplomatic/page_VB_fol_105v_diplomatic.html',
+        interpretative: './data/output_data/interpretative/page_VB_fol_105v_interpretative.html',
+    },{
+        title: 'The Dream of the Rood',
+        page: '106r',
+        diplomatic: './data/output_data/diplomatic/page_VB_fol_106r_diplomatic.html',
+        interpretative: './data/output_data/interpretative/page_VB_fol_106r_interpretative.html',
+    }];
+
     var mockThumb1 = [{
         id: 'VB_fol_104v',
         page: '104v',
@@ -154,6 +176,10 @@ angular.module('evtviewer.dataHandler')
 
     parsedData.getText1 = function() {
         return mockText1;
+    };
+
+    parsedData.getText2 = function() {
+        return mockText2;
     };
 
     parsedData.getThumb = function() {
