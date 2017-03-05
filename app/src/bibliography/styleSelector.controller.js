4dde4630076422a2aa2ee2b0b53bf02d568253e2
Maurizio Ricci <maurizioricci95@gmail.com>
2017-03-05 11:22:00 +0100

    Added timeout to selectController and default value to the bibliographic select style
    Added timeout to selectController in order to gain some time to
    instanciate all evt-bibl-ref controller

--- app/src/bibliography/styleSelector.controller.js
+++ app/src/bibliography/styleSelector.controller.js
@@ -1,8 +1,13 @@
 angular.module('evtviewer.dataHandler')
 
-.controller('StyleSelectorCTRL', function($scope,config) {
+.controller('StyleSelectorCTRL', function($scope,config,$timeout) {
 	$scope.notify = function(selectedStyle){
 		$scope.$emit('styleChangedEmit', {message: selectedStyle});
 	}
 	$scope.styles = config.allowedBibliographicStyles;
-});
+	//all'inizio diamo un pochetto di tempo per istanziare gli altri controller riceventi della direttiva evt-bibl-ref
+	$timeout(function(){
+	//diamo un primo valore alla select e notifichiamo, non voglio abusare di $watch quando si può mettere ng-change	
+	$scope.selectedStyle = $scope.styles[0];	
+	$scope.notify($scope.selectedStyle);},1000);
+	});
