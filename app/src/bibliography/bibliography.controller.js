427bea8c5de430d3a4ee117b8ac5f2201c62d691
Maurizio Ricci <maurizioricci95@gmail.com>
2017-03-13 16:18:03 +0100

    Introduced alphabetic sorting
    if author's surname is given it will be the first element to be sorted,
    otherwise author's name will be taken. So if both surname's are give
    they will be matched against each other otherwise it may be possibile
    that a surname will be matched against a name. System tries his to
    provide the best sort

--- app/src/bibliography/bibliography.controller.js
+++ app/src/bibliography/bibliography.controller.js
@@ -19,4 +19,26 @@ angular.module('evtviewer.bibliography')
 	  $scope.pubblicationType = function(BiblElement) {
 		  return evtBibliographyParser.getType(BiblElement);
 		  }
+		  
+	  $scope.myComparator = function(author1,author2) {
+		//il sistema fa del suo meglio, ordina per cognome (se noto) oppure per nome  
+		author1=author1.value;
+		author2=author2.value;
+		var el1,el2;
+		if(author1 !== undefined){
+			if (author1.surname !== '')el1=author1.surname;
+			else if (author1.name !== '')el1=author1.name;
+		}
+		else{
+			return 1;
+		}
+		if(author2 !== undefined){
+			if (author2.surname !== '')el2=author2.surname;
+			else if (author2.name !== '')el2=author2.name;
+		}
+		else{
+			return -1;
+		}
+		return el1.localeCompare(el2);
+	  };		  
    });
