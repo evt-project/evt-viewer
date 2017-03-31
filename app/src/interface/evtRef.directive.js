a1ae720ef9f0dc18ca28ce568438e11e3aca0734
Maurizio Ricci <maurizioricci95@gmail.com>
2017-03-31 11:50:15 +0200

    First attemp to use template file for ref directive

--- app/src/interface/evtRef.directive.js
+++ app/src/interface/evtRef.directive.js
@@ -1,48 +1,23 @@
 angular.module('evtviewer.dataHandler')
 
-/*/.directive('evtRefAttr', function(parsedData, evtHighlight, evtInterface, evtDialog, $timeout, evtCommunication) {
-	return {
-		restrict: 'A',
-		link: function(scope, element, attr) {/*/
-			/*/element.on('click', function() {
-				if (attr.type !== 'doc') {
-					/*/
-					/*/Cliccando, guardiamo il valore di type e se non è un riferimento interno allora:
-						passiamo a evtHighlight l'id dell'entrata da evidenziare (ci penserà il template della bibliografia al resto)
-						apriamo il dialog con tipo globalInfo
-						scegliamo di visualizzare come pannello iniziale quello della bibliografia/*/
-					/*/
-					var targetEl = $(attr.target);
-					if(false && targetEl && targetEl > 0) {
-						evtInterface.updateSecondaryContentOpened(' ');
-						evtDialog.openByType('globalInfo');
-						evtInterface.setHomePanel(evtInterface.getTabContainerPanel().bibliography.name);
-						evtHighlight.setHighlighted(attr.target);
-						//dopo 2s viene rimosso l'attributo highlight
-						$timeout(function() {
-							evtHighlight.setHighlighted('');
-						}, 2000);
-					}
-					else {
-						evtCommunication.err('Could not find bibliography referement','405');
-					}
-				}
-			});/*/
-	/*/	}
-	}
-});/*/
 
 .directive('ref', function (parsedData, evtHighlight, evtInterface, evtDialog, $timeout, evtCommunication) {
 	return {//rivedere dipendenze
 		restrict: 'C',
 		scope: {
-					target : '@'
+					target : '@',
+					type   : '@'
 				},
 		replace: true,
 		transclude: true,
 		link: function (scope, iElement, iAttrs) {
 					// scope.href = scope.target;
+					    scope.vm = {
+							type    : scope.type,
+							target  : scope.target
+            };
 				},
+				templateUrl: 'src/interface/evtRef.dir.tmpl.html',
 		compile: function(element, attrs) {
 			//se c'è type dentro <ref>, allora questa necessita di span e non di ancore
 			if(typeof attrs.type !== 'undefined' && attrs.type !== '') {
