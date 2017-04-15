c3fdc56dae39825858c283b6c0bd871c70dda0fd
Maurizio Ricci <maurizioricci95@gmail.com>
2017-04-15 15:11:01 +0200

    Totally rewrote sorting comparator
    Who knows how the previous comparator got the job done

--- app/src/bibliography/bibliography.controller.js
+++ app/src/bibliography/bibliography.controller.js
@@ -50,14 +50,14 @@ angular.module('evtviewer.bibliography')
     vm.biblRefsCollection = parsedData.getBibliographicRefsCollection();
 
     vm.getFormattedBibl = function(biblId) {
-        var biblElement = parsedData.getBibliographicRefById(biblId);
+        var biblElement = vm.getBibliographicRefById(biblId);
         if (!biblElement.outputs[vm.initialSelectedStyle]) {
             evtBibliographyParser.formatResult(vm.initialSelectedStyle, biblElement);
         }
         return biblElement.outputs[vm.initialSelectedStyle];
     };
     vm.pubblicationType = function(biblId) {
-        var biblElement = parsedData.getBibliographicRefById(biblId),
+        var biblElement = vm.getBibliographicRefById(biblId),
             type = evtBibliographyParser.getType(biblElement);
         type = type ? type : 'unknown';
         return type;
@@ -67,7 +67,7 @@ angular.module('evtviewer.bibliography')
         return (evtHighlight.getHighlighted() !== '' && evtHighlight.getHighlighted() === entryId);
     };
 
-    vm.myComparator = function(biblId) {
+/*     vm.myComparator = function(biblId) {
         var result = '',
             biblElement = parsedData.getBibliographicRefById(biblId);
         //surname/name/forename sorting
@@ -95,6 +95,56 @@ angular.module('evtviewer.bibliography')
         else if (vm.selectedSorting === vm.sortBy.Year) {
             result = biblElement.date;
         }
+		else if (vm.selectedSorting === vm.sortBy.Title) {
+			result = biblElement.titleAnalytic !== '' ? biblElement.titleAnalytic : biblElement.titleMonogr;
+		}
         return result;
-    }
+    } */
+	
+	vm.getBibliographicRefById =function(biblId){
+		return parsedData.getBibliographicRefById(biblId);;
+	}
+	
+	vm.myComparator = function(biblId1,biblId2) {
+        var result1 = result2 = '';
+		//sorting by author
+		if (vm.selectedSorting === vm.sortBy.Author) {
+            if (typeof biblId1.value.author !== 'undefined' && biblId1.value.author.length > 0) {
+                //first try to compare according to author's surname
+                if (biblId1.value.author[0].surname !== '') {
+                    result1 = biblId1.value.author[0].surname;
+                }
+            }	
+			if (typeof biblId2.value.author !== 'undefined' && biblId2.value.author.length > 0) {
+                //first try to compare according to author's surname
+                if (biblId2.value.author[0].surname !== '') {
+                    result2 = biblId2.value.author[0].surname;
+                }
+            }
+			//if both doesn't have surname use name, so the remainig item without surnames will be sorted using it's name property
+			if (result1 === '' && result2 === '' && 
+			typeof biblId1.value.author !== 'undefined' && biblId1.value.author.length > 0 &&
+			typeof biblId2.value.author !== 'undefined' && biblId2.value.author.length > 0) {
+				result1 = biblId1.value.author[0].name;
+				result2 = biblId2.value.author[0].name;
+			}
+		}
+		//sorting by year
+		else if (vm.selectedSorting === vm.sortBy.Year) {
+			if (typeof biblId1.value.date !== 'undefined'){
+                if (biblId1.value.date !== '') {
+                    result1 = Number(biblId1.value.date);
+                }
+			}
+			if (typeof biblId2.value.date !== 'undefined') {
+                if (biblId2.value.date !== '') {
+                    result2 = Number(biblId2.value.date);
+                }
+			}
+        }	
+		return result1.toString().localeCompare(result2.toString());
+	}
+	
+	
+
 });
