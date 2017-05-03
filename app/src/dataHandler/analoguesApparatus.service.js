d9ecb7ea0d07512b42efb707b519abd819954427
Chiara Martignano <chiaramartignano@gmail.com>
2017-05-03 12:47:58 +0200

    Fixed the header of the analogues apparatus entry.

--- app/src/dataHandler/analoguesApparatus.service.js
+++ app/src/dataHandler/analoguesApparatus.service.js
@@ -82,58 +82,63 @@ angular.module('evtviewer.dataHandler')
 
     apparatus.getHeader = function (analogue, scopeWit) {
         var content = analogue.content || [];
-        var result = '';
+        var result = '<span class="analogue-header">';
         for (var i in content) {
             if (typeof content[i] === 'string') {
-                result += ' '+content[i];
+                result += '<span class="textNode inAnalogueHeader">'+content[i]+'</span>';
             } else {
                 var skip = ['EVT-POPOVER', 'lb', 'ptr', 'link', 'linkGrp', 'pb'];
                 if (skip.indexOf(content[i].tagName) >= 0) {
                     result += '';
                 } else if (content[i].type === 'app') {
                     var t = apparatus.getAppText(content[i], scopeWit);
-                    result += t;
+                    result += '<span class="app inAnalogueHeader">'+t+'</span>';
                 } else if (content[i].type === 'analogue') {
-                    result += ' (('+apparatus.getHeader(content[i], scopeWit)+'))';
+                    result += '<span class="subAnalogue inAnalogueHeader">(('+apparatus.getHeader(content[i], scopeWit)+'))</span>';
                 } else if (content[i].type === 'quote') {
-                    result += ' (('+evtSourcesApparatus.getQuote(content[i], scopeWit)+'))';
+                    result += '<span class="quote inAnalogueHeader">'+evtSourcesApparatus.getQuote(content[i], scopeWit)+'</span>';
+                } else if (content[i].type === 'analogueContent') {
+                    result += apparatus.getHeader(content[i], scopeWit);
                 } else if (content[i].content !== undefined) {
                     result += apparatus.getText(content[i]);
                 }
             }
         }
+        result += '</span>';
         return result;
     }
 
     //Eventualmente aggiungere parametro stringa per il valore della class di span (tipo 'author' o 'textNode')
     apparatus.getText = function(entry) {
-        var result = '';
-        var content = entry.content;
+        var elemName = entry.tagName || 'textNode',
+            result = '<span class="'+elemName+' inAnalogueHeader">',
+            content = entry.content;
         if (content !== undefined) {
             for (var i = 0; i < content.length; i++) {
                 if (typeof content[i] === 'string') {
-                    result += ' '+content[i];
+                    result += content[i];
                 } else if (content[i].content !== undefined) {
                     result += apparatus.getText(content[i]);
                 }
             }
-        }        
+        }
+        result += '</span>';        
         return result;
     }
 
-     apparatus.getAppText = function(entry, scopeWit){
-            var result = '';
-            if (scopeWit === ''
-                || scopeWit === undefined
-                || entry._indexes.witMap[scopeWit] === undefined) {
-                    var lem = entry.lemma;
-                    result += apparatus.getText(entry.content[lem]);
-                } else {
-                    var rdg = entry._indexes.witMap[scopeWit];
-                    result += apparatus.getText(entry.content[rdg]);
-                }
-            return result;
+    apparatus.getAppText = function(entry, scopeWit) {
+        var result = '';
+        if (scopeWit === ''
+            || scopeWit === undefined
+            || entry._indexes.witMap[scopeWit] === undefined) {
+            var lem = entry.lemma;
+            result += apparatus.getText(entry.content[lem]);
+        } else {
+            var rdg = entry._indexes.witMap[scopeWit];
+            result += apparatus.getText(entry.content[rdg]);
         }
+        return result;
+    }
 
     return apparatus;
 });
