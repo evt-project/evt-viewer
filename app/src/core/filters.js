26bcb468fbd6d7416b5ed486a391106b89bb21dd
Chiara Di Pietro <dipi.chiara@gmail.com>
2017-06-12 13:23:52 +0200

    Added support for nested <listPlace>s
    - Added function to support properly nested lists (for now tested only with TEI <listPlace>)
    - Added camelToSpaces AngularJS fitler
    -

--- app/src/core/filters.js
+++ app/src/core/filters.js
@@ -5,4 +5,10 @@ angular.module('evtviewer.core')
 		var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
 		return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
 	}
+})
+
+.filter('camelToSpaces', function() {
+	return function(input, all) {
+		return (!!input) ? input.replace(/\W+/g, ' ').replace(/([a-z\d])([A-Z])/g, '$1 $2') : '';
+	}
 });
--- app/src/dataHandler/namedEntitiesParser.service.js
+++ app/src/dataHandler/namedEntitiesParser.service.js
@@ -66,7 +66,7 @@ angular.module('evtviewer.dataHandler')
 									var el = {};
 									
 									if (contentDef.indexOf(child.tagName) >= 0) { 
-										el = parseEntity(child);
+										el = parseEntity(child, listsToParse[i].contentDef, listsToParse[i].listDef);
 										parsedData.addNamedEntityInCollection(collection, el, el.id.substr(0, 1).toLowerCase());
 									}
 								}
@@ -80,7 +80,7 @@ angular.module('evtviewer.dataHandler')
 		console.log('## parseEntities ##', parsedData.getNamedEntitiesCollection());
 	};
 
-	var parseEntity = function(nodeElem) {
+	var parseEntity = function(nodeElem, contentDef, listDef) {
 		var elId = nodeElem.getAttribute(idAttributeDef) || evtParser.xpath(nodeElem);
 		var el = {
 			id         : elId,
@@ -93,21 +93,99 @@ angular.module('evtviewer.dataHandler')
 		};
 		el.label = elId; //TEMP
 
-		angular.forEach(nodeElem.childNodes, function(child){
+		angular.forEach(nodeElem.childNodes, function(child) {
+			// Each child node of XML node will be saved in a JSON structure that looks like this:
+			// element: {
+			//		content : {
+			//			'childNode_1': { 'text': 'Text with HTML of child 1', 'attributes': [ ] }, 
+			//			'childNode_2': { 'text': 'Text with HTML of child 2', 'attributes': [ ] }, 
+			//			'childNode_n': { 'text': 'Text with HTML of child n', 'attributes': [ ] },
+			//			_indexes: [ 'childNode_1', 'childNode_2', 'childNode_n' ]
+			//		}
+			// }
+			// The generic XML parser will transform the content of each node in an HTML element 
+			// with Tag Name as Class Name
 			if (child.nodeType === 1) {
 				if (el.content[child.tagName] === undefined) {
 					el.content[child.tagName] = [];
 					el.content._indexes.push(child.tagName);
 				}
-				var parsedChild = evtParser.parseXMLElement(child, child, 'evtNote');
+				var parsedChild;
+				if (contentDef.indexOf('<'+child.tagName+'>') >= 0) {
+					parsedChild = NEparser.parseSubEntity(child, contentDef, listDef);
+				} else {
+					parsedChild = evtParser.parseXMLElement(child, child, 'evtNote');
+				}
 				el.content[child.tagName].push({
 					text: parsedChild ? parsedChild.innerHTML : child.innerHTML,
 					attributes: evtParser.parseElementAttributes(child) 
 				}); 
 			}
 		});
-
 		return el;
+	};
+
+	NEparser.parseSubEntity = function(nodeElem, contentDef, listDef) {
+		var newNodeElem = document.createElement('evt-named-entity-ref'),
+			entityRef = nodeElem.getAttribute('ref'),
+			entityId = entityRef ? entityRef.replace('#', '') : undefined;
+		if (entityId && entityId !== '') {
+			newNodeElem.setAttribute('data-entity-id', entityId);
+		}
+		var listType = nodeElem.tagName ? nodeElem.tagName : 'generic';
+		newNodeElem.setAttribute('data-entity-type', listType);
+
+		var entityContent = '';
+		for (var i = 0; i < nodeElem.childNodes.length; i++) {
+			var childElement = nodeElem.childNodes[i].cloneNode(true),
+				parsedXmlElem;
+
+			if (childElement.nodeType === 1 && listDef.toLowerCase().indexOf('<' + childElement.tagName.toLowerCase() + '>') >= 0 ) {
+				parsedXmlElem = NEparser.parseNamedEntitySubList(childElement, childElement, 'evtNote');
+			} else {
+				parsedXmlElem = evtParser.parseXMLElement(childElement, childElement, 'evtNote');
+			} 
+			newNodeElem.appendChild(parsedXmlElem);
+		}
+		return newNodeElem;
+	};
+
+	/* ******************* */
+	/* parseNamedEntitySubList(docDOM) */
+	/* **************************************************************************** */
+	/* Function to parse an XML element representing a named entity sub list	    */
+	/* and transform it into an unordered list with attributes as titles 			*/
+	/* @doc -> XML to be parsed                                                  	*/
+	/* @entityNode -> Node to be transformed										*/
+	/* @skip -> names of sub elements to skip from transformation					*/
+	/* **************************************************************************** */
+	// It will replace the node @entityNode
+	// and replace it with a new ul element
+	NEparser.parseNamedEntitySubList = function(doc, entityNode, skip) {
+		var newNodeElem = document.createElement('span'),
+			entityHeadElem = document.createElement('span'),
+			headTextContent = '';
+		newNodeElem.className = entityNode.tagName.toLowerCase();
+		entityHeadElem.className = entityNode.tagName.toLowerCase() + '-attributes';
+
+		for (var i = 0; i < entityNode.attributes.length; i++) {
+			var attrib = entityNode.attributes[i];
+			if (attrib.specified) {
+				newNodeElem.setAttribute('data-' + attrib.name, attrib.value);
+				headTextContent += evtParser.camelToSpace(attrib.value) + ', ';
+			}
+		}
+		if (headTextContent !== '') {
+			entityHeadElem.textContent = headTextContent.slice(0, -2);
+			newNodeElem.appendChild(entityHeadElem);
+		}
+
+		for (var i = 0; i < entityNode.childNodes.length; i++) {
+			var childElement = entityNode.childNodes[i].cloneNode(true),
+				parsedXmlElem = evtParser.parseXMLElement(doc, childElement, skip);
+			newNodeElem.appendChild(parsedXmlElem);
+		}
+		return newNodeElem;
 	};
 
 	var parseCollectionData = function(el, defCollection) {
