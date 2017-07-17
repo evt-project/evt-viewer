446f0e3ab8e953c2c035da5a30ab2733b6fae8bc
Chiara Di Pietro <dipi.chiara@gmail.com>
2017-07-18 01:33:01 +0200

    Added documentation main page

--- app/src/evtviewer.js
+++ app/src/evtviewer.js
@@ -7,23 +7,6 @@
  * @description
  * # evtviewer
  * Main module of the application. 
- * A different module has been defined for each interface element. For each of them it is possible to have:
- * - a configuration file where the values of some constants are defined
- * - provider/service
- * - directive
- * - controller (where everything concerning the single instance of the directive is defined)
- * 
- * # Module organization
- * Some modules, especially those used to manage data, are defined exclusively by a service.
- * For the implementation of the services, provider was mainly used for modules that need to be instantiated more than once.
- * This choice was first driven by the fact that the provider can be injected into other modules depending from it during the configuration phase.
- * Moreover, from a theoretical point of view, it was possible to create components in a programmatic manner and 
- * to give to all functions defined in the service access to an object that is not directly defined by a directive.
- *
- * # Styling
- *
- * With regard to the graphic style, rules have been defined in SCSS and grouped in an external folder "styles". 
- * {@link http://susy.oddbird.net/ Susy} framework was chosen as a grid-layout engine.
  *
  * @requires {@link https://docs.angularjs.org/api/ngAnimate ngAnimate}
  * @requires {@link https://docs.angularjs.org/api/ngCookies ngCookies}
