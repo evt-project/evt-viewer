a794686b07df16f42f92425ed3f2d9feffa71f1e
Chiara Martignano <chiaramartignano@gmail.com>
2017-07-24 17:50:21 +0200

    Added doc for quote module (*testing*)

--- app/src/quote/quote.js
+++ app/src/quote/quote.js
@@ -4,6 +4,17 @@
  * @module evtviewer.quote
  * @description 
  * # evtviewer.quote
- * TODO: Add Description!
+ * Module containing the classes that determine how quotations inside the text are shown to the user and how to access the corresponding information.
+ * 
+ * ## Services
+ * - {@link evtviewer.quote.evtQuote evtQuote} expands the scope of 
+ * {@link evtviewer.quote.directive:evtQuote evtQuote} directive 
+ * and stores it. The expanded scope is aaved inside the memory as long as the directive is instantiated.
+ *
+ * ## Directives
+ * - {@link evtviewer.quote.directive:quote quote} is custom directive that will highlight the quotations within the running text and allow the user to click on them to access the corresponding sources apparatus entries.
+ *
+ * ## Controllers
+ * - {@link evtviewer.quote.controller:QuoteCtrl QuoteCtrl}: controller for the {@link evtviewer.quote.directive:quote evtQuote} directive.
 **/
 angular.module('evtviewer.quote', []);
