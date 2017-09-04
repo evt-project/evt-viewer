4cfcd27ac833975f529ee1f9e4ae29360b87b135
Chiara Di Pietro <dipi.chiara@gmail.com>
2017-09-04 15:54:57 +0200

    Added docs for apparatuses module

--- app/src/apparatuses/apparatuses.controller.js
+++ app/src/apparatuses/apparatuses.controller.js
@@ -4,14 +4,28 @@
  * @name evtviewer.apparatuses.controller:apparatusesCtrl
  * @description 
  * # apparatusesCtrl
- * TODO: Add description and list of dependencies!
- * The controller for the {@link evtviewer.apparatuses.directive:evtApparatuses evtApparatuses} directive. 
-**/
+ * This is the controller for the {@link evtviewer.apparatuses.directive:evtApparatuses evtApparatuses} directive. 
+ *
+ * @requires $timeout
+ * @requires $scope
+ * @requires evtviewer.apparatuses.evtApparatuses
+ *
+ * @author CM
+ **/
 angular.module('evtviewer.apparatuses')
 
 .controller('apparatusesCtrl', function($timeout, evtApparatuses, $scope) {
 	var vm = this;
 
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#setCurrentApparatus
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Set current visible apparatus.
+     * @param {string} appId Identifier of apparatus to be set as current one
+     */
 	this.setCurrentApparatus = function(appId) {
 		if (appId !== vm.currentApparatus && vm.apparatuses[appId]) {
 			vm.loading = true;
@@ -22,31 +36,81 @@ angular.module('evtviewer.apparatuses')
 			});
 		}
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#getCurrentApparatus
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Retrieve the current apparatus.
+     * @returns {string} Identifier of current apparatus
+     */
 	this.getCurrentApparatus = function() {
 		return vm.currentApparatus;
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#toggleAppStructure
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Change layout structure of apparatuses box.
+	 * @param {string} appStructure Label of structure to be set (handled values are 'tabs' and 'subBoxes')
+     */
 	this.toggleAppStructure = function(appStructure) {
 		if (vm.appStructure !== appStructure) {
 			vm.appStructure = appStructure;
 		}
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#toggleOpenApparatus
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Change the opened apparatus.
+     * @param {string} apparatus Identifier of apparatus to be set as the opened one
+     */
 	this.toggleOpenApparatus = function(apparatus) {
 		if (vm.openApparatus !== apparatus) {
 			vm.openApparatus = apparatus;
 		}
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#getVisibleList
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Retrieve the list of currently visible entries of a particular apparatus.
+     * @param {string} app Identifier of apparatus to handle
+     * @return {array} List of visibile entries for given apparatus
+     */
 	this.getVisibleList = function(app) {
 		return vm.apparatuses[app] ? vm.apparatuses[app].visibleList : [];
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#getAppList
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Retrieve the complete list of entries in a particular apparatus.
+     * @param {string} app Identifier of apparatus to handle
+     * @return {array} Complete list of entries for given apparatus
+     */
 	this.getAppList = function(app) {
 		return vm.apparatuses[app] ? vm.apparatuses[app].list : [];
 	};
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#loadMoreElements
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * Show more entries for current apparatus. This function is used with the plugin
+ 	 * {@link https://sroze.github.io/ngInfiniteScroll/documentation.html infinite-scroll}.
+     */
 	this.loadMoreElements = function() {
 		var appId = vm.currentApparatus,
             last = vm.apparatuses[appId].visibleList.length,
@@ -59,7 +123,14 @@ angular.module('evtviewer.apparatuses')
             i++;
         }
     };
-
+	/**
+     * @ngdoc method
+     * @name evtviewer.apparatuses.controller:apparatusesCtrl#destroy
+     * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+     *
+     * @description
+     * <p>Remove instance from saved instances in {@link evtviewer.apparatuses.evtApparatuses evtApparatuses} provider.</p>
+     */
 	this.destroy = function() {
 		evtApparatuses.destroy(this.uid);
 	};
--- app/src/apparatuses/apparatuses.directive.js
+++ app/src/apparatuses/apparatuses.directive.js
@@ -4,12 +4,21 @@
  * @name evtviewer.apparatuses.directive:evtApparatuses
  * @description 
  * # evtApparatuses
- * TODO: Add description!
- * It uses the {@link evtviewer.apparatuses.controller:apparatusesCtrl apparatusesCtrl} controller.
+ * <p>Custom elements shaped and styled as a {@link evtviewer.box.directive:box box} which will show
+ * the list of all critical apparatuses (critical entries, parallel texts, sources, etc.), properly
+ * divided into tabs.</p>
+ * <p>It is strictly connected to main box: when the user clicks on an apparatus entry, the main edition text will scroll
+ * to the referenced portion of text, and viceversa.</p>
+ * <p>It uses the {@link evtviewer.apparatuses.controller:apparatusesCtrl apparatusesCtrl} controller.</p>
  *
  * @scope
  * @param {string=} currentApparatus id of current apparatus shown
  *
+ * @requires $timeout
+ * @requires evtviewer.apparatuses.evtApparatuses
+ * @requires evtviewer.interface.evtInterface
+ * @requires evtviewer.criticalApparatusEntry.evtCriticalApparatusEntry
+  *
  * @restrict E
 **/
 angular.module('evtviewer.apparatuses')
@@ -31,7 +40,17 @@ angular.module('evtviewer.apparatuses')
                     currentApparatuses.destroy();
                 }
             });
-
+            /**
+             * @ngdoc method
+             * @name evtviewer.apparatuses.controller:apparatusesCtrl#scrollToAppEntry
+             * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+             *
+             * @description
+             * <p>Scroll list of critical entries to reach a particular one. </p>
+             * <p>If the entry we want to scroll to is not inside the visible list of elements,
+             * it will load other elements until it finds it.</p>
+             * @param {string} appId Identifier of critical apparatus entry to handle
+             */
             scope.vm.scrollToAppEntry = function(appId) {
                 var appIndex = 'criticalApparatus';
                 if (currentApparatuses.apparatuses[appIndex]) {
@@ -59,7 +78,17 @@ angular.module('evtviewer.apparatuses')
                     });
                 }
             };
-
+            /**
+             * @ngdoc method
+             * @name evtviewer.apparatuses.controller:apparatusesCtrl#scrollToQuotesEntry
+             * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+             *
+             * @description
+             * <p>Scroll list of sources entries to reach a particular one. </p>
+             * <p>If the entry we want to scroll to is not inside the visible list of elements,
+             * it will load other elements until it finds it.</p>
+             * @param {string} quoteId Identifier of sources apparatus entry to handle
+             */
             scope.vm.scrollToQuotesEntry = function(quoteId) {
                 var appIndex = 'sources';
                 if (currentApparatuses.apparatuses[appIndex]) {
@@ -87,7 +116,17 @@ angular.module('evtviewer.apparatuses')
                     });
                 }
             };
-
+            /**
+             * @ngdoc method
+             * @name evtviewer.apparatuses.controller:apparatusesCtrl#scrollToAnaloguesEntry
+             * @methodOf evtviewer.apparatuses.controller:apparatusesCtrl
+             *
+             * @description
+             * <p>Scroll list of sources entries to reach a particular one. </p>
+             * <p>If the entry we want to scroll to is not inside the visible list of elements,
+             * it will load other elements until it finds it.</p>
+             * @param {string} analogueId Identifier of analogues apparatus entry to handle
+             */
             scope.vm.scrollToAnaloguesEntry = function(analogueId) {
                 var appIndex = 'analogues';
                 if (scope.vm.apparatuses[appIndex]) {
