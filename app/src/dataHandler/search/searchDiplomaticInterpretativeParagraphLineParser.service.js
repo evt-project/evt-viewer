9be7955555c43330dbebba51247a3ad2524126e9
Giulia Cacioli <giulia.cacioli@gmail.com>
2018-05-12 16:44:38 +0200

    Add paragraphs handler

--- app/src/dataHandler/search/searchDiplomaticInterpretativeParagraphLineParser.service.js
+++ app/src/dataHandler/search/searchDiplomaticInterpretativeParagraphLineParser.service.js
@@ -1,5 +1,5 @@
 angular.module('evtviewer.dataHandler')
-   .factory('EvtSearchDiploInterprParLineParser', ['evtSearchDocument', 'evtDiplomaticEditionHandler', 'evtInterpretativeEditionHandler', 'XPATH', function (evtSearchDocument, evtDiplomaticEditionHandler, evtInterpretativeEditionHandler, XPATH) {
+   .factory('EvtSearchDiploInterprParLineParser', ['evtSearchDocument', 'evtParagraphHandler', 'XPATH', function (evtSearchDocument, evtParagraphHandler, XPATH) {
       function DiplomaticInterpretativeParLineParser(xmlDocBody) {
          this.parsedElementsForIndexing = {};
          this.xmlDocBody = xmlDocBody;
@@ -38,9 +38,6 @@ angular.module('evtviewer.dataHandler')
             currentPage,
             currentPageId,
             pageId = 1,
-            paragraph,
-            parId = 1,
-            documentToIndex = {},
             documentsToIndex = {},
             
             node = parLineNodes.iterateNext();
@@ -56,43 +53,7 @@ angular.module('evtviewer.dataHandler')
                   pageId++;
                },
                'p': function () {
-                  var diplomaticNodes = evtDiplomaticEditionHandler.getParagraphDiplomaticNodes(xmlDocDom, node, ns, nsResolver),
-                     interpretativeNodes = evtInterpretativeEditionHandler.getParagraphInterpretativeNodes(xmlDocDom, node, ns, nsResolver);
-                  
-                  for(var i = 0; i < diplomaticNodes.length;) {
-                     var currentParDiplomaticNodes,
-                        currentParInterpretativeNodes;
-                     
-                     if(diplomaticNodes[i].nodeName === 'pb') {
-                        currentPage = evtSearchDocument.getCurrentPage(diplomaticNodes[i]);
-                        currentPageId = evtSearchDocument.getCurrentPageId(diplomaticNodes[i], pageId);
-                        pageId++;
-                        diplomaticNodes.splice(0, 1);
-                        interpretativeNodes.splice(0, 1);
-                     }
-                     paragraph = evtSearchDocument.getParagraph(node, parId);
-   
-                     documentToIndex.xmlDocTitle = currentXmlDoc.title;
-                     documentToIndex.xmlDocId = currentXmlDoc.id;
-                     documentToIndex.paragraph = paragraph;
-                     documentToIndex.page = currentPage;
-                     documentToIndex.pageId = currentPageId;
-                     documentToIndex.docId = documentToIndex.xmlDocId + '-' + documentToIndex.page + '-' + documentToIndex.paragraph;
-   
-                     currentParDiplomaticNodes = evtSearchDocument.getCurrentPageParNodes(xmlDocDom, diplomaticNodes);
-                     currentParInterpretativeNodes = evtSearchDocument.getCurrentPageParNodes(xmlDocDom, interpretativeNodes);
-   
-                     documentToIndex.content = {
-                        diplomatic: evtSearchDocument.getContent(currentParDiplomaticNodes, 'diplomatic'),
-                        interpretative: evtSearchDocument.getContent(currentParInterpretativeNodes, 'interpretative')
-                     };
-                     
-                     documentsToIndex[documentToIndex.docId] = documentToIndex;
-                     currentParDiplomaticNodes = [];
-                     currentParInterpretativeNodes = [];
-                     documentToIndex = {};
-                  }
-                  parId++;
+                  documentsToIndex = evtParagraphHandler.getParagraphInfo(xmlDocDom, currentXmlDoc, node, currentPage, currentPageId, pageId, documentsToIndex, ns, nsResolver);
                },
                'l': function() {
                   console.log();
