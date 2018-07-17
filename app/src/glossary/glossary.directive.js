7cf2cc92b7793b1ca254b52e6565996012ed8ba4
Unknown <alessandro.difante@gmail.com>
2018-07-17 16:26:44 +0200

    Added click event on element with "w" class.

--- app/src/glossary/glossary.directive.js
+++ app/src/glossary/glossary.directive.js
@@ -3,16 +3,25 @@ var app = angular.module('evtviewer.glossary', [])
 
 app.directive('titolo', function(){
 	return{
-	restrict: "EAC", 
-	templateUrl: "/src/glossary/glossary.dir.tmpl.html",
-	link: function ($scope, element, attrs){
-		var documentResult = document.getElementsByClassName("w");
-      	console.log('document.getElementsByClassName: ', documentResult);
-      	
-		if (documentResult.length>0){
-			alert("dentro");}
-		
-      	//console.log('document.getElementsByClassName: ', documentResult[0]);
+		restrict: "A", 
+		//templateUrl: "/src/glossary/glossary.dir.tmpl.html",
+		link: function ($scope, element, attrs){
+			element.ready(function(){
+				console.log("document ready");
+				var documentResult = document.getElementsByClassName("w");
+	      		console.log('document.getElementsByClassName: ', documentResult);
+	      		setTimeout(function(){ console.log("lunghezza", documentResult.length);
+	      			for (i=0;i<documentResult.length;i++){
+	      			//console.log("nel for");
+	      			documentResult[i].addEventListener("click", function(event){
+    					alert(this.innerHTML);
+    				event.stopPropagation();
+	      			})
+	      		}
+				}, 100);
+	      		
+	      		
+			})
+		}
 	}
-}
 });
