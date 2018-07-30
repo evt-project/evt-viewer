27ba1f245727ac3d54235956dd80af7db2a84abc
Alessandro Di Fante <alessandro.difante@gmail.com>
2018-07-30 17:50:14 +0200

    Added URL generation in glossary directive.

--- app/src/glossary/glossaryWord.directive.js
+++ app/src/glossary/glossaryWord.directive.js
@@ -1,6 +1,6 @@
 angular.module('evtviewer.glossary')
 
-.directive('w', function(parsedData) {
+.directive('w', function(parsedData, config) {
     return {
         restrict: 'C',
         link: function($scope, element, attrs) {
@@ -11,12 +11,18 @@ angular.module('evtviewer.glossary')
                     var word = element.text().trim();
                     var wordTS = word.toUpperCase();
                     var glossaryEntry = parsedData.getGlossaryEntryById(wordTS);
-                    
+                    var glossUrl
+                    if (config.glossaryUrl == "data/glossDucange.xml"){
+                        glossUrl = "http://ducange.enc.sorbonne.fr/" + word;
+                    }
+                    else if (config.glossaryUrl == "data/glossarioXML.xml"){
+                        glossUrl = "http://bosworth.ff.cuni.cz/finder/3/" + word;
+                    }
                     if (glossaryEntry) {
                         console.log(glossaryEntry);
-                        alert('DO SOMETHING FOR "' + glossaryEntry.content);
+                        alert('DO SOMETHING FOR "' + glossaryEntry.content + " " + glossUrl);
                     } else {
-                        alert('Entry not found');
+                        alert('Entry not found in glossary' + glossUrl);
                     }
                 }
             });
