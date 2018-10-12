ba320fa0bfa55dddf464ad10cb94effd9ba8df63
Giulia Cacioli <giulia.cacioli@gmail.com>
2018-10-12 14:50:55 +0200

    Fixed splitPages function when there are more documents

--- app/src/dataHandler/parser.service.js
+++ app/src/dataHandler/parser.service.js
@@ -886,11 +886,12 @@ angular.module('evtviewer.dataHandler')
 					// Split pages works only on diplomatic/interpretative edition
 					// In critical edition, text will be splitted into pages for each witness
 					config.defaultEdition = 'diplomatic';
-               		var pages = parsedData.getPages();
+					var pages = parsedData.getPages();
+					var newDocPages = newDoc.pages;
 					angular.forEach(angular.element(element).find(defContentEdition),
 						function(editionElement) {
 							//editionElement.innerHTML = parser.splitLineBreaks(element, defContentEdition);
-							parser.splitPages(pages, editionElement, newDoc.value, defContentEdition);
+							parser.splitPages(pages, editionElement, newDoc.value, defContentEdition, newDocPages);
 						});
 				}
 			});
@@ -959,27 +960,42 @@ angular.module('evtviewer.dataHandler')
 
      * @author CDP
      */
-	parser.splitPages = function(pages, docElement, docId, defContentEdition) {
+	parser.splitPages = function(pages, docElement, docId, defContentEdition, currentDocPages) {
 		var matchOrphanText = '<body(.|[\r\n])*?(?=<pb)',
 			sRegExInputOrphanText = new RegExp(matchOrphanText, 'ig'),
 			matchesOrphanText = docElement.outerHTML.match(sRegExInputOrphanText);
+      
+      var match = '<pb(.|[\r\n])*?(?=(<pb|<\/' + defContentEdition + '>))';
+      var sRegExInput = new RegExp(match, 'ig');
+      var matches = docElement.outerHTML.match(sRegExInput);
+      var pageId;
+      
 		if (matchesOrphanText && matchesOrphanText.length > 0) {
 			var previousDoc = parsedData.getPreviousDocument(docId);
+			
 			if (previousDoc && previousDoc.pages && previousDoc.pages.length > 0) {
 				var parentPageId = previousDoc.pages[previousDoc.pages.length - 1];
+				var currentDocPos = pages.length - currentDocPages.length;
+				
 				if (parentPageId && parentPageId !== '') {
-					parsedData.setPageText(parentPageId, docId, 'original', matchesOrphanText[0]);
+				   var matchId = 0;
+               for (var j = currentDocPos; j < pages.length; j++) {
+                  pageId = pages[j];
+                  if (pageId && pageId !== '') {
+                     parsedData.setPageText(pageId, docId, 'original', matches[matchId]);
+                     matchId++;
+                  }
+               }
 				}
 			}
-		}
-		var match = '<pb(.|[\r\n])*?(?=(<pb|<\/' + defContentEdition + '>))';
-		var sRegExInput = new RegExp(match, 'ig');
-		var matches = docElement.outerHTML.match(sRegExInput);
-		for (var i = 0; i < pages.length; i++) {
-			var pageId = pages[i];
-			if (pageId && pageId !== '') {
-				parsedData.setPageText(pageId, docId, 'original', matches[i]);
-			}
+         else {
+            for (var i = 0; i < pages.length; i++) {
+               pageId = pages[i];
+               if (pageId && pageId !== '') {
+                  parsedData.setPageText(pageId, docId, 'original', matches[i]);
+               }
+            }
+         }
 		}
 	};
 	/**
