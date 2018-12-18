17257cf9e4a0cfcd210c89da33ddbc575930e3bb
federicaspinelli <federicaspinelli@msn.com>
2018-12-18 10:14:00 +0100

    Features San matteo

--- app/src/imageViewer/imageViewer.service.js
+++ app/src/imageViewer/imageViewer.service.js
@@ -11,53 +11,29 @@
         var options = {
             id: "osd-img",
             prefixUrl: "bower_components/openseadragon/built-openseadragon/openseadragon/images/",
-            //data/test-img/vb/VB_fol_104v_big.jpg
-            tileSources:[] , // immagine per rotulo Alba
-            //tileSources: "data/test-img/quamusdzi/QuamusImgManuscript.dzi", // immagine per Marocco
-            //overlays: [{
-            //    id: 'example-overlay',
-            //    x: 0.03,
-            //    y: 0.20,
-            //    width: 0.15,
-            //    height: 0.45,
-            //    className: 'nohighlight'
-            //}],            
-            /*tileSources: ["data/bellinidzi/4-26giugno1834a.dzi", // immagini per bellini
-                            "data/bellinidzi/4-26giugno1834b.dzi"],
-            
-                            overlays: [{
-                                id: 'example-overlay',
-                                x: 0.26,
-                                y: 0.39,
-                                width: 0.22,
-                                height: 0.27,
-                                className: 'nohighlight'
-                            }], */
-    
-            //sequenceMode: true,
-            //degrees:90,
-            //toolbar:"toolbar-div",
+            tileSources: "data/tails/sanmatteo.dzi", //Senza questo nome visualizzao errore HTTP 404 attempting to load tilesource
+            // tileSources: "data/tails/sanmatteo.dzi", immagine per San Matteo
             showRotationControl: true,                
             showNavigator: true,
             //navigatorId: "navscroll",
             //visibilityRatio: 0.8,
             visibilityRatio: 1, 
-            defaultZoomLevel: 0.7,
+            defaultZoomLevel: 0,
             panHorizontal: true,
             constrainDuringPan: true,
             //minZoomLevel: 0.8,
-            minZoomLevel: 0.5,
-            maxZoomLevel: 2.0,
-    
+            minZoomLevel: 1,
+            maxZoomLevel: 8.0,   
             wrapVertical: false,
             //navigatorPosition: "ABSOLUTE",
             //navigatorTop: "0",
             //navigatorLeft: "94%",
             //navigatorHeight: "100%",
             //navigatorWidth: "6%",
-            //navigatorTop:"1%",
+            navigatorTop:"0",
+            navigatorBottom:"0",
             //navigatorLeft:"94%",
-            //navigatorHeight:"95%",
+            navigatorHeight:"100%",
             //navigatorWidth:"6%"
          };
          // {id:"box_body_mainImage",prefixUrl:"images/",tileSources:"data/tails/scaled_70_verticale.dzi",showNavigator:!0,visibilityRatio:1,defaultZoomLevel:1,panHorizontal:!0,constrainDuringPan:!0,minZoomLevel:1,wrapVertical:!0,navigatorPosition:"ABSOLUTE",navigatorTop:"1%",navigatorLeft:"98%",navigatorHeight:"95%",navigatorWidth:"8%"}
