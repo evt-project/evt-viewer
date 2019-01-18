6868f2fc7178f16eacac76f46dfe72857158c8c3
Chiara Martignano <chiaramartignano@gmail.com>
2019-01-18 16:31:10 +0100

    Reorder cinfguration variables

--- app/src/core/GLOBALDEFAULTCONF.constant.js
+++ app/src/core/GLOBALDEFAULTCONF.constant.js
@@ -48,13 +48,81 @@ angular.module('evtviewer.core')
 			active: true
 		}
 	},
-	// Default:
+	
+	indexTitle: 'EVT Critical Viewer',
+	webSite: '',
+	logoUrl: '',
+
+	defaultLanguage: "en",
+	languages: ['en', 'it'],
+
+		// Default:
 	// <pre> configUrl: '../../config/config.json' </pre>
 	configUrl: '../../config/config.json',
-  dataUrl: '',
-
-	logoUrl: '',
-	defaultLanguage: "en",
+	dataUrl: '',
+  analoguesUrl: '',
+	sourcesUrl: '',
+	sourcesTextsUrl: '',
+	
+	defaultViewMode: 'readingTxt',
+	availableViewModes: [{
+			label    : 'READING_TEXT',
+			icon     : 'reading-txt',
+			viewMode : 'readingTxt',
+			visible  : true
+	},
+	{
+			label    : 'MULTIPLE_RECENSIONS',
+			icon     : 'mode-versions',
+			viewMode : 'versions',
+			visible  : true
+	},
+	{
+			label    : 'IMAGE_TEXT',
+			icon     : 'mode-imgTxt',
+			viewMode : 'imgTxt',
+			visible  : true
+	},
+	{
+			label    : 'TEXT_TEXT',
+			icon     : 'mode-txtTxt',
+			viewMode : 'txtTxt',
+			visible  : false
+	},
+	{
+			label    : 'COLLATION',
+			icon     : 'mode-collation',
+			viewMode : 'collation',
+			visible  : true
+	},
+	{
+			label    : 'SOURCE_TEXT',
+			icon     : 'mode-srcTxt',
+			viewMode : 'srcTxt',
+			visible  : true
+	}],
+	editionType: 'critical',
+	defaultEdition: 'critical',
+	showEditionLevelSelector: false,
+	availableEditionLevel: [{
+		value: 'critical',
+		label: 'Critical',
+		title: 'Critical edition',
+		visible: true
+	}, {
+		value: 'diplomatic',
+		label: 'Diplomatic',
+		title: 'Diplomatic edition',
+		visible: false
+	}, {
+		value: 'interpretative',
+		label: 'Interpretative',
+		title: 'Interpretative edition',
+		visible: false
+	}],
+														
+	mainDocId: '',
+	showDocumentSelector: true,
 
 	enableXMLdownload: true,
 	downloadableFiles: [
@@ -75,126 +143,39 @@ angular.module('evtviewer.core')
 			}
 		}
 	],
-    // sourcesUrl //
-    // Url of the XML file encoding the list of all the bibliographic references for the sources apparatus.//
-  sourcesUrl       : '',
-    // sourcesTextsUrl //
-    // Path of the folder containing the xml files of the sources texts //
-  sourcesTextsUrl : '',
-    //analoguesUrl//
-    //Url of the XML file encoding the list of all the bibliographic references for the analogues apparatus.//
-  analoguesUrl     : '',
 
-  preferredWitness: '',
-	skipWitnesses: '',
-
-	indexTitle: 'EVT Critical Viewer',
-	webSite: '',
-
-	editionType: 'critical',
-
-	defaultEdition: 'critical',
-
-	showEditionLevelSelector: false,
-	availableEditionLevel: [{
-		value: 'critical',
-		label: 'Critical',
-		title: 'Critical edition',
-		visible: true
-	}, {
-		value: 'diplomatic',
-		label: 'Diplomatic',
-		title: 'Diplomatic edition',
-		visible: false
-	}, {
-		value: 'interpretative',
-		label: 'Interpretative',
-		title: 'Interpretative edition',
-		visible: false
-	}],
-	
-	mainDocId: 'tr',
-
-	showDocumentSelector: true,
-
-	defaultViewMode: 'readingTxt',
-	availableViewModes: [{
-                                label    : 'READING_TEXT',
-                                icon     : 'reading-txt',
-                                viewMode : 'readingTxt',
-                                visible  : true
-                            },
-                            {
-                                label    : 'MULTIPLE_RECENSIONS',
-                                icon     : 'mode-versions',
-                                viewMode : 'versions',
-                                visible  : true
-                            },
-                            {
-                                label    : 'IMAGE_TEXT',
-                                icon     : 'mode-imgTxt',
-                                viewMode : 'imgTxt',
-                                visible  : true
-                            },
-                            {
-                                label    : 'TEXT_TEXT',
-                                icon     : 'mode-txtTxt',
-                                viewMode : 'txtTxt',
-                                visible  : false
-                            },
-                            {
-                                label    : 'COLLATION',
-                                icon     : 'mode-collation',
-                                viewMode : 'collation',
-                                visible  : true
-                            },
-                            {
-                                label    : 'SOURCE_TEXT',
-                                icon     : 'mode-srcTxt',
-                                viewMode : 'srcTxt',
-                                visible  : true
-                            }],
-	elementsWithNumbers: ['seg'],
-	toolHeatMap: true,
 	toolPinAppEntries: false,
+	toolHeatMap: true,
 	toolImageTextLinking: true,
+
+	// NAMED ENTITIES
+	namedEntitiesSelector: true,
+	namedEntitiesToHandle: [],
+	otherEntitiesToHandle: [],
+	listsMainContentDef: '<back>',
 
 	listDef: '<listWit>, <listChange>',
 	versionDef: '<witness>, <change>',
 	fragmentMilestone: '<witStart>, <witEnd>',
 	lacunaMilestone: '<lacunaStart>, <lacunaEnd>',
+	quoteDef: '',
+	analogueDef: '',
 	possibleVariantFilters: 'type, cause, hand',
 	possibleLemmaFilters: 'resp, cert',
 	notSignificantVariant: '<orig>, <sic>, [type=orthographic]',
 
+  preferredWitness: '',
+	skipWitnesses: '',
+	witnessesGroups: [],
+	versions : ["rec2", "rec1", "rec3"],
+
+	alwaysPositiveApparatus: false,
+	showReadingExponent: true,
+	showInlineCriticalApparatus: true,
+	showInlineSources: false,
+	showInlineAnalogues: false,
 	loadCriticalEntriesImmediately: true,
 	maxWitsLoadTogether: 5,
-	alwaysPositiveApparatus: false,
-
-    /*Versions*/
-    /*Array to encode cases of double or multiple redactions of the text*/
-    /*The array collects the id used inside of the XML file as values of*/
-    /*the @ana attribute on the rdgGrp element.                         */
-    /*The first id will correspond to the default version.              */
-    versions : [],
-
-    /*witnessesGroups*/
-    /*Used to divide the readings of all critical apparatus entries into groups.*/
-    /*The witnesses property is required in order to have the partition.        */
-    /*The groupName is optional: if set the name will be displayed.             */
-    witnessesGroups: [],
-
-    /*Definition of the element used within the XML file to encode quotes for the sources apparatus.*/
-    quoteDef    : '',
-    /*Definition of the element used within the XML file to encode passages for the analogues apparatus.*/
-    analogueDef : '',
-
-    showReadingExponent: true,
-    showInlineCriticalApparatus: true,
-    showInlineSources: false,
-    showInlineAnalogues: false,
-
-
 
 	variantColors: {},
 	filterColors: {},
@@ -202,11 +183,12 @@ angular.module('evtviewer.core')
 		'rgb(235, 77, 153)', 'rgb(252, 144, 172)', 'rgb(171, 99, 219)', 'rgb(67, 135, 217)',
 		'rgb(163, 207, 81)', 'rgb(238, 194, 66)', 'rgb(228, 99, 220)', 'rgb(124, 113, 232)'
 	],
-
 	variantColorLight: 'rgb(208, 220, 255)',
 	variantColorDark: 'rgb(101, 138, 255)',
 	heatmapColor: 'rgb(255, 108, 63)',
 	xsltUrl: '',
+
+	elementsWithNumbers: ['seg'],
 
 	// BIBLIOGRAPHY
 	defaultBibliographicStyle: 'Chicago',
@@ -236,11 +218,5 @@ angular.module('evtviewer.core')
 		DESC: 'BIBLIOGRAPHY.DESC'
 	},
 
-    // NAMED ENTITIES
-    namedEntitiesSelector: true,
-    namedEntitiesToHandle: [],
-		otherEntitiesToHandle: [],
-		listsMainContentDef: '<back>',
-
-    languages: ['en', 'it']
+	virtualKeyboardKeys: []
 });
