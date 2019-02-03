eb71a99b47cbe11aedd5cf7000b93d7774c8b865
federicaspinelli <federicaspinelli@msn.com>
2019-02-03 23:39:35 +0100

    update imageViewer.service.js for San Matteo tails

--- app/src/imageViewer/imageViewer.service.js
+++ app/src/imageViewer/imageViewer.service.js
@@ -11,7 +11,7 @@
         var options = {
             id: "osd-img",
             prefixUrl: "bower_components/openseadragon/built-openseadragon/openseadragon/images/",
-            tileSources: "data/tiles/scaled_70_verticale.dzi", // immagine per rotulo Alba
+            tileSources: "data/tails/sanmatteo.dzi", // immagine SanMatteo
             
             //overlays: [{
             //    id: 'example-overlay',
@@ -21,17 +21,6 @@
             //    height: 0.45,
             //    className: 'nohighlight'
             //}],            
-            /*tileSources: ["data/bellinidzi/4-26giugno1834a.dzi", // immagini per bellini
-                            "data/bellinidzi/4-26giugno1834b.dzi"],
-            
-                            overlays: [{
-                                id: 'example-overlay',
-                                x: 0.26,
-                                y: 0.39,
-                                width: 0.22,
-                                height: 0.27,
-                                className: 'nohighlight'
-                            }], */
     
             //sequenceMode: true,
             //degrees:90,
@@ -46,8 +35,7 @@
             constrainDuringPan: true,
             //minZoomLevel: 0.8,
             minZoomLevel: 1,
-            maxZoomLevel: 8.0,
-    
+            maxZoomLevel: 8.0,   
             wrapVertical: false,
             //navigatorPosition: "ABSOLUTE",
             //navigatorTop: "0",
