945137b24fbd9398186bbb5d06842cd824c884db
angelodel80 <angelodel80@gmail.com>
2019-02-15 19:37:44 +0100

    prima versione div hotspot popup con pulsante close

--- app/src/imageViewer/viewerOSD.service.js
+++ app/src/imageViewer/viewerOSD.service.js
@@ -5,6 +5,7 @@
       .service('imageViewerHandler', function (evtInterface, imageScrollMap, overlayHandler) {
          const ImageNormalizationCoefficient = 3500;
          const YminPan = 0.5;
+         var divHotSpotToggle = false;
 
          var viewerHandler = this;
 
@@ -235,7 +236,7 @@
 
          viewerHandler.showHotSpot = function (zones) {
             //showHotSpots -> for(){showHotSpot}
-            var toggle = false;
+            //var toggle = false;
             console.log('in showHotSpot di ViewerHandler', zones);
 
             var rectObjs = [];
@@ -265,12 +266,22 @@
                hrefElt.className = 'hotspot';
                hrefElt.dataset.id = id;
                hrefElt.dataset.content = content;
-               hrefElt.onclick = function () {
-                  toggle = showDivHotSpot(toggle, this);
-               }; //function(){console.log('hot spot');};
-               hrefElt.onmouseleave = function () {
-                  toggle = hiddenDivHotSpot(toggle, this);
+               hrefElt.onmouseover = function () {
+                  viewerHandler.viewer.zoomPerClick = 1;
+                  
                };
+               hrefElt.onmouseout = function(){
+                  viewerHandler.viewer.zoomPerClick = 1.5;
+               };
+               hrefElt.onclick = function(){
+                  showDivHotSpot(this);
+               };
+               
+               //function(){console.log('hot spot');};
+               // hrefElt.onmouseleave = function () {
+               //    toggle = hiddenDivHotSpot(toggle, this);
+               // };
+
                hrefElts.push(hrefElt);
             }
             console.log('hotspots: ', hrefElts);
@@ -286,8 +297,8 @@
             }
          };
 
-         var showDivHotSpot = function (toggle, elem) {
-            if (!toggle) {
+         var showDivHotSpot = function (elem) {
+            if (!divHotSpotToggle) {
                console.log("elem id", elem.id);
                var _$elem = $(elem);
                var x = _$elem.position().left;
@@ -322,7 +333,35 @@
                var divTitleElt = document.createElement('div');
                divTitleElt.id = 'div-title-hotspot-overlay_selected-' + elem.dataset.id;
                divTitleElt.className = 'hotspot-dida-title';
-               divTitleElt.innerHTML = 'HotSpot n.: '+elem.dataset.id;
+               //divTitleElt.innerHTML = 'HotSpot n.: '+elem.dataset.id;
+
+               //<div class="PopupCloser" onclick="HideAnnHS('Ann_VB_hs_106r_01')"><i class="fa fa-times"></i></div>
+
+               var divCloser = document.createElement('div');
+               divCloser.className='PopupCloser';
+               divCloser.dataset.id = elem.dataset.id;
+               
+               divCloser.onclick = function(){
+                  viewerHandler.viewer.zoomPerClick = 1.5;      
+                  hiddenDivHotSpot(this);
+            };
+
+               var closeIcon = document.createElement('i');
+               closeIcon.className = 'fa fa-times';
+
+               closeIcon.onmouseover = function(){
+                  viewerHandler.viewer.zoomPerClick = 1;
+                  console.log('closeIcon.onmouseover zoom per click: ', viewerHandler.viewer.zoomPerClick)
+               };
+               closeIcon.onmouseout = function(){
+                  viewerHandler.viewer.zoomPerClick = 1.5;
+                  console.log('closeIcon.onmouseout zoom per click: ', viewerHandler.viewer.zoomPerClick)
+               };
+
+               
+
+               divCloser.appendChild(closeIcon);
+               divTitleElt.appendChild(divCloser);
 
                var divBodyElt = document.createElement('div');
                divBodyElt.id = 'div-body-hotspot-overlay_selected-' + elem.dataset.id;
@@ -342,13 +381,13 @@
 
                console.log(OSDOverlay.element);
                viewerHandler.viewer.addOverlay(OSDOverlay);
-               toggle = !toggle;
+               divHotSpotToggle = !divHotSpotToggle;
             }
-            return toggle;
+            return divHotSpotToggle; // non dovrebbe servire più
          };
 
-         var hiddenDivHotSpot = function (toggle, elem) {
-            console.log('hiddenDivHotSpot: ' + toggle);
+         var hiddenDivHotSpot = function (elem) {
+            console.log('hiddenDivHotSpot: ' + divHotSpotToggle);
             try {
                var id = elem.dataset.id;
                viewerHandler.viewer.removeOverlay('div-hotspot-overlay_selected-' + id);
@@ -356,8 +395,8 @@
                console.error('no hotspot overlay', error);
             }
 
-            toggle = false;
-            return toggle;
+            divHotSpotToggle = false;
+            return divHotSpotToggle;
 
 
          };
@@ -407,12 +446,9 @@
                   }
 
                } 
-
-
-
-               
+              
                //alert('overlay da img');
-            }
+            };
 
             elt.onmouseout = function(){
                console.log('de spegnere riga corrispondente a '+ this.dataset['lb']);
@@ -437,11 +473,11 @@
                //alert('overlay da img');
 
                }
-            }
+            };
 
             elt.onclick = function(){
                overlayHandler.test('funge on click');
-            }
+            };
 
             viewerHandler.viewer.addOverlay({
                element: elt,
@@ -449,7 +485,7 @@
             });
 
             return elt;
-         }
+         };
 
 
 
