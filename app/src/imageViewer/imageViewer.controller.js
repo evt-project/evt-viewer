5db667694862a0b58fc1a4433d19971ed7436ff6
angelodel80 <angelodel80@gmail.com>
2019-03-02 20:46:54 +0100

    aggiunta file provider e modulo controller

---
+++ app/src/imageViewer/imageViewer.controller.js
@@ -0,0 +1,17 @@
+// (function(){
+
+//     // angular.module('evtviewer.imageViewer')
+
+//     // .controller('imageViewerCtrl',['$scope', function($scope) {
+//     // //   var vm = this;
+//     // //   vm.test = 'ciao';
+//     // $scope.test = 'CIAO!';
+
+            
+//     //      console.log("caricato controller evtviewer.imageViewer", $scope);
+//     //   }]);
+
+//       console.log('creato controller imageViewer')
+
+// })();
+
