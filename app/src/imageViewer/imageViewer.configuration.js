b114ec1dc2536ccd50fbe8ae2fc7a9eb81f7bb8f
angelodel80 <angelodel80@gmail.com>
2019-03-02 20:46:54 +0100

    aggiunta file provider e modulo controller

---
+++ app/src/imageViewer/imageViewer.configuration.js
@@ -0,0 +1,11 @@
+// angular.module('evtviewer.imageViewer')
+// //    .constant('VIEWERDEFAULTS', {
+// //       osd: {
+// //          y: 0.5
+// //       }
+// //    })
+   
+// //    .config(function(osdProvider,configProvider,VIEWERDEFAULTS) {
+// //         var defaults = configProvider.makeDefaults('osd', VIEWERDEFAULTS);
+// //         osdProvider.setDefaults(defaults);
+// //    });
