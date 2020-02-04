3973248183ab57495ef2b4b9f611007ce98945aa
federicaspinelli <federicaspinelli@msn.com>
2020-02-04 10:19:38 +0100

    Add new version of js plugins

--- app/src/3dhop/3dhop.directive.js
+++ app/src/3dhop/3dhop.directive.js
@@ -2,7 +2,7 @@
  * @ngdoc directive
  * @module evtviewer.3dhop
  * @name evtviewer.3dhop.directive:evtTreDHOP
- * @description 
+ * @description
  * # evtTreDHOP
  * It uses the {@link evtviewer.3dhop.controller:TreDHOPCtrl TreDHOPCtrl} controller.
  * The initial scope is expanded in {@link evtviewer.3dhop.evtTreDHOP evtTreDHOP} provider.
@@ -18,29 +18,29 @@ angular.module('evtviewer.3dhop', [])
 		scope: {
 			canvas: '@',
 		},
-		transclude: true,		
+		transclude: true,
 		controllerAs: 'vm',
-		//controller: "TreDHOPCtrl",		
+		//controller: "TreDHOPCtrl",
 		templateUrl: 'src/3dhop/3dhop.directive.tmpl.html',
-		
+
 		link: function (scope, element, attrs) {
-			
+
 			var pluginFolder = 'js-plugins/3dhop/';
 			var jsFiles = ['spidergl.js', 'jquery.js', 'presenter.js', 'nexus.js', 'ply.js', 'trackball_turntable.js',
 				'trackball_turntable_pan.js', 'trackball_pantilt.js', 'trackball_sphere.js', 'init.js']
-			
+
 			var loadFiles = function(fileIndex) {
 				$ocLazyLoad.load(pluginFolder + jsFiles[fileIndex]).then(function() {
 					if (jsFiles[fileIndex + 1]) {
-						loadFiles(fileIndex + 1);																		
+						loadFiles(fileIndex + 1);
 					} else {
-						initializeViewer();						
+						initializeViewer();
 					}
 				console.log('FInished loading js files')
-				})				
+				})
 			}
 
-			
+
 			/*	$ocLazyLoad.load(pluginFolder +'spidergl.js', 'jquery.js', 'presenter.js', 'nexus.js', 'ply.js', 'trackball_turntable.js', 'trackball_turntable_pan.js', 'trackball_pantilt.js', 'trackball_sphere.js', 'init.js').then(function() {
 					console.log('loaded!!');
 					initializeViewer();
@@ -49,13 +49,13 @@ angular.module('evtviewer.3dhop', [])
 					console.log('errr');
 					console.error(e);
 				})
-			*/						
+			*/
 
 			var initializeViewer = function() {
 				init3dhop();
-				setup3dhop();				
+				setup3dhop();
 			};
-			
+
 			loadFiles(0);
 
 			var presenter = null;
@@ -118,7 +118,7 @@ angular.module('evtviewer.3dhop', [])
 				} catch (e) {
 					console.log(e)
 				}
-			}			
-		}		
+			}
+		}
 	};
 });
