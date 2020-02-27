129d2a74aa2dd4807c180e767485ca7d9d811f61
federicaspinelli <federicaspinelli@msn.com>
2020-02-27 11:17:07 +0100

    Add functions to manage camera ligthing

--- app/src/3dhop/3dhop.controller.js
+++ app/src/3dhop/3dhop.controller.js
@@ -2,12 +2,5 @@ angular.module('evtviewer.3dhop')
 
 .controller('TreDHOPCtrl', function($log, $scope, $http) {
 	var vm = this;
-   var _console = $log.getInstance('3dhop');
-   $http.get("array.json")
-      .success(function (response) {
-         $scope.options = response.dati[0];
-         console.log("Prova");
-         var objurl = response.dati[0];
-         console.log(objurl);
-      });
+   var _console = $log.getInstance('tdhop');
 });
