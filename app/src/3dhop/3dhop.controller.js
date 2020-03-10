b2cca215966412e5d96d65ece30de74378581895
federicaspinelli <federicaspinelli@msn.com>
2020-03-10 11:38:38 +0100

    Handle 3dhop js files to solve undefined functions

--- app/src/3dhop/3dhop.controller.js
+++ app/src/3dhop/3dhop.controller.js
@@ -3,4 +3,6 @@ angular.module('evtviewer.3dhop')
 .controller('TreDHOPCtrl', function($log, $scope, $http) {
 	var vm = this;
    var _console = $log.getInstance('tdhop');
+   var tying= JSON.parse(JSON.stringify(config.witnessesGroups));
+   console.log("Caricamento CONFIG " +tying);
 });
