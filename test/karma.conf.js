e7c14d5edf636eb11eead2c5e18da2fde90a7b7b
ChiaraDipi <dipi.chiara@gmail.com>
2020-07-03 18:25:14 +0200

    Remove bower references from karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,14 +18,14 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-messages/angular-messages.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
+      'angular/angular.js',
+      'angular-mocks/angular-mocks.js',
+      'angular-animate/angular-animate.js',
+      'angular-cookies/angular-cookies.js',
+      'angular-messages/angular-messages.js',
+      'angular-resource/angular-resource.js',
+      'angular-route/angular-route.js',
+      'angular-sanitize/angular-sanitize.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
